db7ea070d173a928147fd475ca2e92e49956caba
Paddy Duff <paddy.duff31@googlemail.com>
2023-11-08 13:11:10 +0000

    read from assembly

--- Dte.Common/Content/CustomMessageEmail.cs
+++ Dte.Common/Content/CustomMessageEmail.cs
@@ -1,15 +1,21 @@
 using System.IO;
+using System.Linq;
 
 namespace Dte.Common.Content
 {
     public static class CustomMessageEmail
     {
         private const string BodyPlaceholder = "###BODY_REPLACE###";
-        private const string HtmlTemplatePath = "./EmailTemplate.html";
         
         public static string GetCustomMessageHtml(string bodyContent)
         {
-            var htmlContent = File.ReadAllText(HtmlTemplatePath);
+            // get the html template as an embedded resource, so that it is included in the assembly
+            var assembly = typeof(CustomMessageEmail).Assembly;
+            var resourceName = assembly.GetManifestResourceNames()
+                .Single(str => str.EndsWith("EmailTemplate.html")); 
+            var stream = assembly.GetManifestResourceStream(resourceName);
+            var reader = new StreamReader(stream);
+            var htmlContent = reader.ReadToEnd();
 
             htmlContent = htmlContent
                 .Replace(BodyPlaceholder, bodyContent);
